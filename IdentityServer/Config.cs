51b579d8071f79a5a39b579b363ac5bfcf72bba9
Roman Rodriguez <romedsoft@gmaill.com>
2023-04-21 12:06:04 -0600

    updating Identity server version and adding test Clients

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -26,6 +27,17 @@ namespace IdentityServer
 
         public static IEnumerable<Client> Clients =>
             new Client[]
-            { };
+            {
+                new Client
+                {
+                    ClientName = "testclient",  
+                    ClientId = "testclient",
+                    AllowedGrantTypes = GrantTypes.Code , 
+                    RedirectUris =  new List<string> { "http://localhost:8100/signin-oidc" },
+                    AllowedScopes = { IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile }
+                    ,ClientSecrets = {new Secret("UserClientSecret".Sha512()) }
+                    ,RequireConsent = true
+                }
+            };
     }
 }
